cbf4c0c8411d146373d76c420abc1e5c868f25f2
aleksandr <alexander.ivanov@jugru.org>
2021-02-12 13:13:10 +0300

    - adding maven wrapper - adding simple app with lots of starters

---
+++ src/test/java/ru/alsi/jwtapp/JwtappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alsi.jwtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
